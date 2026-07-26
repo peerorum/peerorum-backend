62c7789f91fa01cd4ba74a4edc9cbd919abd89cf
sjy303256@gmail.com <sjy303256@gmail.com>
2026-07-26 22:05:37 +0900

    chore: 프로젝트 기본 설정 및 의존성 추가

---
+++ peer-orum/src/main/java/io/github/peerorum/peer_orum/PeerOrumApplication.java
@@ -0,0 +1,13 @@
+package io.github.peerorum.peer_orum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerOrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerOrumApplication.class, args);
+	}
+
+}
---
+++ peer-orum/src/test/java/io/github/peerorum/peer_orum/PeerOrumApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.peerorum.peer_orum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerOrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
