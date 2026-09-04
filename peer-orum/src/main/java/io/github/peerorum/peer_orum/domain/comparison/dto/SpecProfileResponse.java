04e297e3295cbebce1921651ee0fbb8c8785f873
sjy303256@gmail.com <sjy303256@gmail.com>
2026-09-04 17:37:19 +0900

    Feat: add extra spec counts and major to SpecProfileResponse

--- peer-orum/src/main/java/io/github/peerorum/peer_orum/domain/comparison/dto/SpecProfileResponse.java
+++ peer-orum/src/main/java/io/github/peerorum/peer_orum/domain/comparison/dto/SpecProfileResponse.java
@@ -9,17 +9,29 @@ import lombok.Getter;
 public class SpecProfileResponse {
     private String anonymousUuid;
     private String virtualNickname;
+    private String major;
     private Double gpa;
     private Integer toeicScore;
     private String desiredJob;
+    private Integer verificationCount;
+    private Integer internCount;
+    private Integer activityCount;
 
     public static SpecProfileResponse from(SpecProfile specProfile) {
+        int certs = specProfile.getCertificates() != null ? specProfile.getCertificates().size() : 0;
+        int interns = specProfile.getInterns() != null ? specProfile.getInterns().size() : 0;
+        int activities = specProfile.getActivities() != null ? specProfile.getActivities().size() : 0;
+
         return SpecProfileResponse.builder()
                 .anonymousUuid(specProfile.getUser().getAnonymousUuid())
                 .virtualNickname(specProfile.getUser().getVirtualNickname())
+                .major(specProfile.getMajor())
                 .gpa(specProfile.getGpa())
                 .toeicScore(specProfile.getToeicScore())
                 .desiredJob(specProfile.getDesiredJob())
+                .verificationCount(certs)
+                .internCount(interns)
+                .activityCount(activities)
                 .build();
     }
 }
